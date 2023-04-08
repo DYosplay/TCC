8ebd3bfb16f1324092131529ae216fc9df027514
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-04-08 17:10:15 -0300

    170b

--- ds_dtw_pytorch_gpu1.py
+++ ds_dtw_pytorch_gpu1.py
@@ -162,7 +162,7 @@ class DsDTW(nn.Module):
         #     eval("self.rnn.bias_ih_l%d"%i)[self.n_hidden:2*self.n_hidden].data.fill_(-1e10) #Initial update gate bias
     
         self.linear = nn.Linear(self.n_hidden, 64, bias=False)
-        self.linear2 = nn.Linear(self.n_hidden, 16, bias=False)
+        self.linear2 = nn.Linear(64, 16, bias=False)
 
         nn.init.kaiming_normal_(self.linear.weight, a=1)
         nn.init.kaiming_normal_(self.linear2.weight, a=1) 
