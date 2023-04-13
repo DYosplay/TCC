a3ccb111b0360e7c9b3564b337e986e8a3117b5d
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-04-13 14:41:13 -0300

    189

--- dsdtw_run.py
+++ dsdtw_run.py
@@ -8,8 +8,8 @@ BATCH_SIZE = 16
 FEATURES = [0,1,2,3,4,5,6,7,8,9,10,11]
 # FEATURES=[0,1,2]
 DATASET_FOLDER = "Data" + os.sep + "DeepSignDB"
-N_EPOCHS = 30
-PARENT_FOLDER = "ds_test188"
+N_EPOCHS = 40
+PARENT_FOLDER = "ds_test189"
 
 FILE = "Data" + os.sep + "DeepSignDB" + os.sep + "Comparison_Files" + os.sep + "TBIOM_2021_Journal" + os.sep + "stylus" + os.sep + "4vs1" + os.sep + "skilled" + os.sep + "Comp_DeepSignDB_skilled_stylus_4vs1.txt"
 FILE2 = "Data" + os.sep + "DeepSignDB" + os.sep + "Comparison_Files" + os.sep + "TBIOM_2021_Journal" + os.sep + "stylus" + os.sep + "4vs1" + os.sep + "skilled" + os.sep + "Comp_eBioSignDS1_W1_skilled_stylus_4vs1.txt"
