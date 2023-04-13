c7885ef51f8783f6447aa115de84dc6f36a33854
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-04-13 13:48:40 -0300

    187t

--- ds_dtw_pytorch_gpu1.py
+++ ds_dtw_pytorch_gpu1.py
@@ -177,7 +177,7 @@ class DsDTW(nn.Module):
         
         # self.new_dtw = new_dtw.SoftDTW(True, gamma=5, normalize=False, bandwidth=0.2)
         # self.new_sdtw_fw = dtw_cuda.DTW(False, normalize=False, bandwidth=1)
-        self.new_dtw = new_soft_dtw.SoftDTW(True, gamma=5, normalize=False, bandwidth=1)
+        self.new_dtw = new_soft_dtw.SoftDTW(True, gamma=5, normalize=False, bandwidth=0.1)
         self.new_sdtw = new_soft_dtw.SoftDTW(True, gamma=5, normalize=False, bandwidth=0.1)
         self.dtw = dtw_cuda.DTW(True, normalize=False, bandwidth=1)
         # self.sdtw = soft_dtw_cuda.SoftDTW(True, gamma=5, normalize=False, bandwidth=0.1)
