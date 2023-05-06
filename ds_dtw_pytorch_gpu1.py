cdc8f2741ee9583b48cb40a4151fe4698c1badf0
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-06 12:17:37 -0300

    245

--- ds_dtw_pytorch_gpu1.py
+++ ds_dtw_pytorch_gpu1.py
@@ -349,7 +349,7 @@ class DsDTW(nn.Module):
             non_zeros = 1
             for g in dist_g:
                 for n in dist_n:
-                    temp = F.relu(g + self.margin - n) * 0.5 + F.relu(self.margin - n) * 0.25 + F.relu(g - self.margin) * 0.25
+                    temp = F.relu(g + self.margin - n) * 0.5 + F.relu(self.margin - n) * 0.25 + F.relu(g + self.margin) * 0.25
                     if temp > 0:
                         # temp += self.margin - n
                         lk += temp
