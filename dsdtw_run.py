3603be1f302770989f48032435fe1c5409d01de9
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-08 09:52:29 -0300

    261

--- dsdtw_run.py
+++ dsdtw_run.py
@@ -10,8 +10,8 @@ FEATURES = [0,1,2,3,4,5,6,7,8,9,10,11]
 DATASET_FOLDER = "Data" + os.sep + "DeepSignDB"
 N_EPOCHS = 500
 GAMMA = 5
-PARENT_FOLDER = "ds_test260"
-LEARNING_RATE = 0.0001
+PARENT_FOLDER = "ds_test261"
+LEARNING_RATE = 0.001
 
 FILE = "Data" + os.sep + "DeepSignDB" + os.sep + "Comparison_Files" + os.sep + "TBIOM_2021_Journal" + os.sep + "stylus" + os.sep + "4vs1" + os.sep + "skilled" + os.sep + "Comp_DeepSignDB_skilled_stylus_4vs1.txt"
 FILE8 = "Data" + os.sep + "DeepSignDB" + os.sep + "Comparison_Files" + os.sep + "TBIOM_2021_Journal" + os.sep + "stylus" + os.sep + "1vs1" + os.sep + "skilled" + os.sep + "Comp_DeepSignDB_skilled_stylus_1vs1.txt"
