6975223f6efd80959628f26dc47875220502b66f
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-08 12:45:20 -0300

    267

--- ds_dtw_pytorch_gpu1.py
+++ ds_dtw_pytorch_gpu1.py
@@ -164,7 +164,7 @@ class DsDTW(nn.Module):
         #     eval("self.rnn.bias_hh_l%d"%i)[self.n_hidden:2*self.n_hidden].data.fill_(-1e10) #Initial update gate bias
         #     eval("self.rnn.bias_ih_l%d"%i)[self.n_hidden:2*self.n_hidden].data.fill_(-1e10) #Initial update gate bias
     
-        self.linear = nn.Linear(self.n_hidden, 16, bias=True)
+        self.linear = nn.Linear(self.n_hidden, 16, bias=False)
         # self.linear2 = nn.Linear(64, 16, bias=False)
 
         nn.init.kaiming_normal_(self.linear.weight, a=1)
@@ -207,13 +207,13 @@ class DsDTW(nn.Module):
             step = (self.ng + self.nf + 1)
             for i in range(0, self.nw):
                 anchor = h[i*step]
-                for j in range(i*step, (i+1)*step):
-                    value, output = ((self.new_sdtw_fw)(anchor[None,], h[j:j+1,]))
-                    output = output[0][1:h.shape[1]+1, 1:h.shape[1]+1].detach().cpu().numpy()        
+                # for j in range(i*step, (i+1)*step):
+                #     value, output = ((self.new_sdtw_fw)(anchor[None,], h[j:j+1,]))
+                #     output = output[0][1:h.shape[1]+1, 1:h.shape[1]+1].detach().cpu().numpy()        
 
-                    output = torch.from_numpy(output).cuda()
+                #     output = torch.from_numpy(output).cuda()
 
-                    output_mask = (((output - torch.min(output)) / (torch.max(output) - torch.min(output))) + 1)
+                #     output_mask = (((output - torch.min(output)) / (torch.max(output) - torch.min(output))) + 1)
                     # output_aux = torch.ones(output.shape).cuda()
 
                     # para a lógica inversa:
@@ -240,7 +240,7 @@ class DsDTW(nn.Module):
                     #     output_mask[r, ck_add]      = value
                     #     output_mask[r, ck_sub]      = value
 
-                    src_masks[j] = output_mask
+                    # src_masks[j] = output_mask
             
             h = self.enc1(src=h, src_mask=src_masks, src_key_padding_mask=(~mask.bool()))
             # h = self.enc2(src=h, src_key_padding_mask=(~mask.bool()))
@@ -248,13 +248,13 @@ class DsDTW(nn.Module):
             src_masks = torch.zeros([h.shape[0], h.shape[1], h.shape[1]], dtype=h.dtype, device=h.device)
             sign = h[-1]
 
-            for i in range(len(h)):
-                value, output = self.new_sdtw_fw(sign[None, ], h[i:i+1, ])
-                output = output[0][1:h.shape[1]+1, 1:h.shape[1]+1].detach().cpu().numpy()        
+            # for i in range(len(h)):
+            #     value, output = self.new_sdtw_fw(sign[None, ], h[i:i+1, ])
+            #     output = output[0][1:h.shape[1]+1, 1:h.shape[1]+1].detach().cpu().numpy()        
 
-                output = torch.from_numpy(output).cuda()
+            #     output = torch.from_numpy(output).cuda()
 
-                output_mask = (((output - torch.min(output)) / (torch.max(output) - torch.min(output))) + 1)
+            #     output_mask = (((output - torch.min(output)) / (torch.max(output) - torch.min(output))) + 1)
                 # output_aux = torch.ones(output.shape).cuda()
 
                 # para a lógica inversa:
@@ -280,7 +280,7 @@ class DsDTW(nn.Module):
                 #     output_mask[r, ck_add]      = value
                 #     output_mask[r, ck_sub]      = value
 
-                src_masks[i] = output_mask
+                # src_masks[i] = output_mask
             
             h = self.enc1(src=h, src_mask=src_masks, src_key_padding_mask=(~mask.bool()))
             # h = self.enc2(src=h, src_key_padding_mask=(~mask.bool()))
