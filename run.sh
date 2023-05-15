f86d878db3b4c5954533a498eaa40e6a5d4ce46e
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-14 22:11:50 -0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=100 -t=ds_test294 -tl=0.5
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=100 -t=ds_test295 -tl=0.7
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=100 -t=ds_test296 -tl=0.3
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=100 -t=ds_test297 -tl=0.8
