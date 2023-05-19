b2eef024278fd917dfeaed748f9583d8e7d9c367
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-19 15:06:55 -0300

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test344 -lr 0.01 -bs 16 -lt=icnn_loss -a 1.0 -p 0.8 -q 0.6 -r 0.1
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test345 -lr 0.01 -bs 16 -lt=icnn_loss -a 1.0 -p 0.6 -q 0.8 -r 0.1
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test346 -lr 0.01 -bs 16 -lt=icnn_loss -a 1.0 -p 0.8 -q 0.8 -r 0.1
-
-
