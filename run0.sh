754be7b848820456f7e56740294ddb55aae1d35c
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-20 08:27:51 -0300

    Add files via upload

--- run0.sh
+++ run0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test347 -lr 0.01 -bs 16 -lt=icnn_loss -a 2.0 -p 0.8 -q 0.6 -r 0.1
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test348 -lr 0.01 -bs 16 -lt=icnn_loss -a 2.0 -p 0.6 -q 0.8 -r 0.1
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test349 -lr 0.01 -bs 16 -lt=icnn_loss -a 2.0 -p 0.8 -q 0.8 -r 0.1
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test353 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 8 -q 8 -r 8
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test354 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 10 -q 10 -r 10
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test355 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 12 -q 12 -r 12
 
 
 
--- run1.sh
+++ run1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test347 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 0.8 -q 0.6 -r 0.1
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test348 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 0.6 -q 0.8 -r 0.1
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test349 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 0.8 -q 0.8 -r 0.1
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test350 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 2 -q 2 -r 2
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test351 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 4 -q 4 -r 4
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test352 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 6 -q 6 -r 6
