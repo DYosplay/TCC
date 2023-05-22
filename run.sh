452bce0d754b32be87dba81982b330c5d39b04b6
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-22 10:48:40 -0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test365 -lr 0.01 -bs 16
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test366 -lr 0.01 -bs 16
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test367 -lr 0.01 -bs 16
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test359 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 2 -q 4 -r 1
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test360 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 4 -q 2 -r 1
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=35 -t=ds_test361 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 6 -q 4 -r 1
 
 
 
--- run1.sh
+++ run1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test368 -lr 0.01 -bs 16
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test369 -lr 0.01 -bs 16 -a 0.1 -b 0.1
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test370 -lr 0.01 -bs 16 -a 0.3 -b 0.3
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test350 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 2 -q 2 -r 2
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test351 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 4 -q 4 -r 4
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test352 -lr 0.01 -bs 16 -lt=icnn_loss -a 0.5 -p 6 -q 6 -r 6
