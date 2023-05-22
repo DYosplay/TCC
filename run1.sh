82f69fb420f70120f83e401594a6d2aa086d16ed
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-22 19:06:08 -0300

    Add files via upload

--- run1.sh
+++ run1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test366 -lr 0.01 -bs 16 -lt=quadruplet_margin -qm 0.4
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test367 -lr 0.01 -bs 16 -lt=quadruplet_margin -qm 0.3
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test368 -lr 0.01 -bs 16 -lt=quadruplet_margin -qm 0.2
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test366 -lr 0.01 -bs 16 -lt=quadruplet_loss -qm 0.4
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test367 -lr 0.01 -bs 16 -lt=quadruplet_loss -qm 0.3
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=35 -t=ds_test368 -lr 0.01 -bs 16 -lt=quadruplet_loss -qm 0.2
