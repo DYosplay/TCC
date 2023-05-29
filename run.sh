2bd0892e87d5474532b9cae13f7e1c67b7b04a7a
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-29 14:16:59 -0300

    488-490

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=10 -t=ds_test482 -lr 0.01 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.9 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=10 -t=ds_test483 -lr 0.01 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.8 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=10 -t=ds_test484 -lr 0.01 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.7 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=12 -t=ds_test488 -lr 0.01 -bs 32 -lt=triplet_mmd -a 0.09 -tm 1 -p 0.85 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=12 -t=ds_test489 -lr 0.01 -bs 32 -lt=triplet_mmd -a 0.08 -tm 1 -p 0.82 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=12 -t=ds_test490 -lr 0.01 -bs 32 -lt=triplet_mmd -a 0.07 -tm 1 -p 0.87 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
