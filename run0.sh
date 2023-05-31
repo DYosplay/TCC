8e711d2b6e9bcffdb951ba509022659095365617
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-05-31 08:33:37 -0300

    Transformer 530-533

--- run0.sh
+++ run0.sh
@@ -1,2 +1,2 @@
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_test526 -lr 0.005 -bs 32 -lt=triplet_mmd -a 0.8 -tm 1 -p 0.8 -q 0.25 -qm 1.5 -dc 0.6 -nlr 0.0000009
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_test527 -lr 0.005 -bs 32 -lt=triplet_mmd -a 0.8 -tm 1 -p 0.8 -q 0.30 -qm 1.5 -dc 0.6 -nlr 0.0000009
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=12 -t=ds_test530 -lr 0.005 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.9 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=12 -t=ds_test531 -lr 0.001 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.9 -q 0.1 -qm 1.5 -dc 0.6 -nlr 0.000009
--- run1.sh
+++ run1.sh
@@ -1,2 +1,2 @@
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=ds_test528 -lr 0.005 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.90 -q 0.2 -qm 1.5 -dc 0.6 -nlr 0.000006
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=ds_test529 -lr 0.005 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.75 -q 0.2 -qm 1.5 -dc 0.6 -nlr 0.000006
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=12 -t=ds_test532 -lr 0.001 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.9 -q 0.2 -qm 1.5 -dc 0.6 -nlr 0.000009
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=12 -t=ds_test533 -lr 0.005 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.9 -q 0.2 -qm 1.5 -dc 0.6 -nlr 0.000009
