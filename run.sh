09d8cba17e72126127765d1e0a4f8d170b4525ef
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-06-10 16:54:09 -0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono12 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 333
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono14 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 888
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono15 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 6666
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono16 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 9999
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=15 -t=ds_mono09 -lr 0.01 -bs 64 -lt=hard_triplet_mmd -dc 0.6 -stop 26 -seed 333 -a 1.0 -tm 1 -p 0.9 -q 0.1 -qm 1.5
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=15 -t=ds_mono10 -lr 0.01 -bs 64 -lt=hard_triplet_mmd -dc 0.6 -stop 26 -seed 888 -a 1.0 -tm 1 -p 0.9 -q 0.1 -qm 1.5
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=15 -t=ds_mono11 -lr 0.01 -bs 64 -lt=hard_triplet_mmd -dc 0.6 -stop 26 -seed 6666 -a 1.0 -tm 1 -p 0.9 -q 0.1 -qm 1.5
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=15 -t=ds_mono12 -lr 0.01 -bs 64 -lt=hard_triplet_mmd -dc 0.6 -stop 26 -seed 9999 -a 1.0 -tm 1 -p 0.9 -q 0.1 -qm 1.5
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono17 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 333
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono18 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 888
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono19 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 6666
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ds_mono20 -lr 0.01 -bs 32 -lt=triplet_mmd -dc 0.9 -stop 26 -seed 9999
