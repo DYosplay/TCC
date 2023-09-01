06960cb9f8b051f1edf650cd81ebb5c5f9e00a52
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-09-01 14:23:28 -0300

    Add files via upload

--- run0.sh
+++ run0.sh
@@ -1,3 +1,3 @@
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python ad_training.py -ep=18 -lf=ds_triplet_mmd_333 -t=adda_004 -lr 0.01 -bs 16 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python ad_training.py -ep=18 -lf=ds_triplet_mmd_333 -t=adda_004 -lr 000001 -bs 16 -stop 26
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python ad_training.py -ep=18 -lf=ds_triplet_mmd_333 -t=adda_005 -lr 0.001 -bs 16 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python ad_training.py -ep=18 -lf=ds_triplet_mmd_333 -t=adda_005 -lr 0000001 -bs 16 -stop 26
