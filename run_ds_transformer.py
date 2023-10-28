499fcf2cb8545b8980ffd844b4c11b22ca79b03f
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-10-28 10:09:26 -0300

    Add files via upload

--- run_ds_transformer.py
+++ run_ds_transformer.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
 	cudnn.benchmark = False
 	cudnn.deterministic = True
 
-	res_folder = "Resultados" + os.sep + args.test_name
+	res_folder = "SVC" + os.sep + args.test_name
 
 	if args.transfer_domain:
 		"""Iniciar treino"""
@@ -257,7 +257,7 @@ if __name__ == '__main__':
 
 	elif not args.evaluate and not args.validate:
 		"""Iniciar treino"""
-		model = DsTransformer(batch_size=args.batch_size, in_channels=len(args.features), dataset_folder=args.dataset_folder, gamma=args.gamma, lr=args.learning_rate, use_mask=args.mask, loss_type=args.loss_type, alpha=args.alpha, beta=args.beta, p=args.p, q=args.q, r=args.r, qm=args.quadruplet_margin, margin = args.margin, decay = args.decay, nlr = args.new_learning_rate, use_fdtw = args.use_fdtw, fine_tuning=args.fine_tuning, early_stop=args.early_stop, z=args.zscore, kernel=args.kernel, mul=args.mul)
+		model = DsTransformer(batch_size=args.batch_size, in_channels=3, dataset_folder=args.dataset_folder, gamma=args.gamma, lr=args.learning_rate, use_mask=args.mask, loss_type=args.loss_type, alpha=args.alpha, beta=args.beta, p=args.p, q=args.q, r=args.r, qm=args.quadruplet_margin, margin = args.margin, decay = args.decay, nlr = args.new_learning_rate, use_fdtw = args.use_fdtw, fine_tuning=args.fine_tuning, early_stop=args.early_stop, z=args.zscore, kernel=args.kernel, mul=args.mul)
 		# model.load_state_dict(torch.load("Resultados/ds_triplet_mmd_333" + os.sep + "Backup" + os.sep + args.weight))
 		print(count_parameters(model))
 		model.cuda()
