fd2fef7f5e5f5e56e7a901f9bb260739b4562601
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-12-11 17:33:46 -0300

    Add files via upload

--- run1.sh
+++ run1.sh
@@ -37,8 +37,8 @@
 #     PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=$result$i -lr 0.01 -bs 32 -lt=triplet_mmd -a 1.0 -tm 1 -p 0.7 -q 0.1 -qm 1.5 -dc 0.9 -stop 26
 # done
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=ctl013 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 2 -p 1 -q 0.1 -r 0.1 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=ctl014 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 1 -p 1 -q 0.1 -r 0.1 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=ctl015 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 2 -p 1 -q 0.05 -r 0.1 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=ctl016 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 1 -p 1 -q 0.1 -r 0.05 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_transformer.py -ep=25 -t=ctl017 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 0.5 -p 1 -q 0.05 -r 0.05 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1  python run_ds_transformer.py -ep=25 -t=ctl013 -lr 0.01 -bs 64 -lt=compact_triplet_mmd -b 1 -a 1 -p 0.3 -r 0.3 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1  python run_ds_transformer.py -ep=25 -t=ctl014 -lr 0.01 -bs 64 -lt=compact_triplet_mmd -b 1 -a 1 -p 0.4 -r 0.4 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1  python run_ds_transformer.py -ep=25 -t=ctl015 -lr 0.01 -bs 64 -lt=compact_triplet_mmd -b 2 -a 1 -p 0.3 -r 0.3 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1  python run_ds_transformer.py -ep=25 -t=ctl016 -lr 0.01 -bs 64 -lt=compact_triplet_mmd -b 2 -a 1 -p 0.2 -r 0.2 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1  python run_ds_transformer.py -ep=25 -t=ctl017 -lr 0.01 -bs 64 -lt=compact_triplet_mmd -b 2 -a 1 -p 0.1 -r 0.1 -stop 26
