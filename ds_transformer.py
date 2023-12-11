2d9d25da69e3b234ac3c43a4b09a29ec38da095e
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2023-12-11 16:32:21 -0300

    Add files via upload

--- ds_transformer.py
+++ ds_transformer.py
@@ -577,6 +577,89 @@ class DsTransformer(nn.Module):
  
         return total_loss + (mmd_loss/comparisons) + var_g         
 
+    def _compact_triplet_mmd(self, data, lens):
+        """ Loss de um batch
+
+        Args:
+            data (torch tensor): Batch composto por mini self.nw mini-batches. Cada mini-batch é composto por 16 assinaturas e se refere a um único usuário e é disposto da seguinte forma:
+            [0]: âncora; [1:6]: amostras positivas; [6:11]: falsificações profissionais; [11:16]: aleatórias 
+            lens (torch tensor (int)): tamanho de cada assinatura no batch
+            n_epoch (int): número da época que se está calculando.
+
+        Returns:
+            torch.tensor (float): valor da loss
+        """
+
+        # data = self.bn(data.transpose(1,2), lens).transpose(1,2)
+
+        step = (self.ng + self.nf + 1)
+        total_loss = 0
+        dists_gs = torch.zeros(self.ng*self.nw, dtype=data.dtype, device=data.device)
+        dists_ns = torch.zeros(self.nf*self.nw, dtype=data.dtype, device=data.device)
+
+        for i in range(0, self.nw):
+            anchor    = data[i * step]
+            positives = data[i * step + 1 : i * step + 1 + self.ng] 
+            negatives = data[i * step + 1 + self.ng : i * step + 1 + self.ng + self.nf]
+
+            len_a = lens[i * step]
+            len_p = lens[i * step + 1 : i * step + 1 + self.ng] 
+            len_n = lens[i * step + 1 + self.ng : i * step + 1 + self.ng + self.nf]
+
+            dist_g = torch.zeros((len(positives)), dtype=data.dtype, device=data.device)
+            dist_n = torch.zeros((len(negatives)), dtype=data.dtype, device=data.device)
+
+            '''Average_Pooling_2,4,6'''
+            for j in range(len(positives)): 
+                dist_g[j] = self.sdtw(anchor[None, :int(len_a)], positives[j:j+1, :int(len_p[j])])[0] / (len_a + len_p[j])
+                dists_gs[i*self.ng + j] = dist_g[j]
+
+            for j in range(len(negatives)):
+                dist_n[j] = self.sdtw(anchor[None, :int(len_a)], negatives[j:j+1, :int(len_n[j])])[0] / (len_a + len_n[j])
+                dists_ns[i*self.nf + j] = dist_n[j]
+
+            only_pos = torch.sum(dist_g) * (self.model_lambda /self.ng)
+
+            # lk = 0
+            # non_zeros = 1
+            # for g in dist_g:
+            #     for n in dist_n[:5]:
+            #         temp = F.relu(g + self.margin - n) * self.p
+            #         if temp > 0:
+            #             lk += temp
+            #             non_zeros+=1
+
+            #     for n in dist_n[5:]:
+            #         temp = F.relu(g + self.quadruplet_margin - n) * (1 - self.p)
+            #         if temp > 0:
+            #             lk += temp
+            #             non_zeros+=1
+            # lv = lk / non_zeros
+            lk_skilled = F.relu(dist_g.unsqueeze(1) + self.margin - dist_n[:5].unsqueeze(0))
+            lk_random = F.relu(dist_g.unsqueeze(1) + self.margin - dist_n[5:].unsqueeze(0))
+            
+            ca = torch.mean(dist_g)
+            cb = torch.mean(dist_n[:5])
+            intra_loss = torch.sum(dist_g - ca)
+            inter_loss = torch.sum(F.relu(self.beta - ((ca - cb).norm(dim=0, p=2))))
+
+            # lk1 = F.relu(dist_g.unsqueeze(1) + self.margin - dist_n[:5].unsqueeze(0)) * self.p
+            # lk2 = F.relu(dist_g.unsqueeze(1) + self.margin - dist_n[5:].unsqueeze(0)) * (1-self.p)
+            # lv = (torch.sum(lk1) + torch.sum(lk2)) / (lk1.data.nonzero(as_tuple=False).size(0) + lk2.data.nonzero(as_tuple=False).size(0) + 1)
+            lv = (torch.sum(lk_skilled) + torch.sum(lk_random)) / (lk_skilled.data.nonzero(as_tuple=False).size(0) + lk_random.data.nonzero(as_tuple=False).size(0) + 1)
+
+            user_loss = lv + intra_loss * self.p + inter_loss * self.r
+
+            total_loss += user_loss
+    
+        total_loss /= self.nw
+       
+        mmd1 = self.mmd_loss(data[0:step - 5], data[step: step*2 - 5]) * self.alpha
+        # var_g = torch.var(dists_gs) * self.q
+
+ 
+        return total_loss + mmd1 # + var_g 
+
     def _compact_triplet_loss(self, data, lens):
         """ Loss de um batch
 
@@ -918,7 +1001,7 @@ class DsTransformer(nn.Module):
         if self.loss_type == 'icnn_loss':
             optimizer = optim.SGD(self.parameters(), lr=self.lr, momentum=0.9)
             lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=self.decay) 
-        elif self.loss_type == 'grl' or self.loss_type == 'triplet_loss' or self.loss_type == 'quadruplet_loss' or self.loss_type == 'hard_triplet_mmd' or self.loss_type == 'triplet_mmd' or self.loss_type == 'triplet_coral' or self.loss_type == 'norm_triplet_mmd' or self.loss_type == 'compact_triplet_loss':
+        elif self.loss_type == 'grl' or self.loss_type == 'triplet_loss' or self.loss_type == 'quadruplet_loss' or self.loss_type == 'hard_triplet_mmd' or self.loss_type == 'triplet_mmd' or self.loss_type == 'triplet_coral' or self.loss_type == 'norm_triplet_mmd' or self.loss_type == 'compact_triplet_loss' or self.loss_type == 'compact_triplet_mmd':
             optimizer = optim.SGD(self.parameters(), lr=self.lr, momentum=0.9)
             lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=self.decay) 
         
@@ -1002,6 +1085,9 @@ class DsTransformer(nn.Module):
                     loss.backward()
                 elif self.loss_type == 'compact_triplet_loss':
                     loss = self._compact_triplet_loss(outputs, length)
+                    loss.backward()
+                elif self.loss_type == 'compact_triplet_mmd':
+                    loss = self._compact_triplet_mmd(outputs, length)
                     loss.backward()
                 else:
                     loss = self._loss(outputs, length)
--- run0.sh
+++ run0.sh
@@ -33,9 +33,9 @@
 #     PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=12 -t=$result$i -aw
 # done
 
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl001 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 3 -p 1 -q 0.8 -r 0.6 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl002 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 2 -p 1 -q 0.8 -r 0.6 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl003 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 1 -p 1 -q 0.8 -r 0.6 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl004 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 3 -p 1 -q 0.2 -r 0.6 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl005 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 2 -p 1 -q 0.2 -r 0.6 -stop 26
-PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl006 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 1 -p 1 -q 0.2 -r 0.6 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl018 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 1 -a 1 -p 0.1 -r 0.1 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl019 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 2 -a 1 -p 0.1 -r 0.1 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl020 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 3 -a 1 -p 0.1 -r 0.1 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl021 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 1 -a 1 -p 0.2 -r 0.2 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl022 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 2 -a 1 -p 0.2 -r 0.2 -stop 26
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_transformer.py -ep=25 -t=ctl023 -lr 0.01 -bs 64 -lt=compact_triplet_loss -b 3 -a 1 -p 0.2 -r 0.2 -stop 26
