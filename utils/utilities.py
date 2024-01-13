542d3dc879659d8f0afa40c05667f0afaaa78e2d
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-01-13 08:53:26 -0300

    Add files via upload

--- utils/utilities.py
+++ utils/utilities.py
@@ -1,7 +1,7 @@
     
 import numpy as np
 from numpy import typing as npt
-from Losses import triplet_loss, triplet_mmd, compact_triplet_mmd, clustering_triplet_mmd, contrastive_loss, triplet_loss_offset, compact_triplet_mmd_offset
+from Losses import triplet_loss, triplet_mmd, compact_triplet_mmd, clustering_triplet_mmd, contrastive_loss, triplet_loss_offset, compact_triplet_mmd_offset, clustering_triplet_loss
 from typing import List, Tuple, Dict, Any
 import torch
 import json
@@ -22,6 +22,8 @@ def define_loss(loss_type : str, ng : int, nf : int, nw : int, margin : torch.nn
         return triplet_loss_offset.Triplet_Loss_Offset(ng=ng, nf=nf, nw=nw, margin=margin, model_lambda=model_lambda,alpha=alpha)
     if loss_type.lower() == "clustering_triplet_mmd_offset":
         return compact_triplet_mmd_offset.Compact_Triplet_MMD_Offset(ng=ng,nf=nf,nw=nw,margin=margin, alpha=alpha, beta=beta, p=p, r=r, mmd_kernel_num=mmd_kernel_num, mmd_kernel_mul=mmd_kernel_mul)
+    if loss_type.lower() == "clustering_triplet_loss":
+        return clustering_triplet_loss.Clustering_Triplet_Loss(ng=ng, nf=nf, nw=nw, margin=margin, model_lambda=model_lambda)
     
     raise NameError("Loss function not found")
 
