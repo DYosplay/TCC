a80a957f7955d79b466dd65150fbaa261258d2d4
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-01-17 22:42:15 -0300

    Add files via upload

--- run2.sh
+++ run2.sh
@@ -1,7 +1,7 @@
 result="CTL_S06_016_"
 for i in {071..105}
 do
-    PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.9 -b 2.7 -p 0.57 -r 0.608 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -t=$result$i -pf=CTL_S06_016_ -wpn=CTL_S06
+    PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.9 -b 2.7 -p 0.57 -r 0.608 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -t=$result$i -pf=CTL_S06_016_ -wpn=CTL_S06
 done
 
 # Gera o sumario dos treinamentos
