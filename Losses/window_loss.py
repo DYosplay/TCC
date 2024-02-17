33f4641a6f5238c32f495ef7888423b08f0ec9f9
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-02-17 14:20:04 -0300

    Add files via upload

---
+++ Losses/window_loss.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from Losses import mmd_loss as mmd
+import DTW.soft_dtw_cuda as soft_dtw
+import numpy as np
+
+class Window_Loss(nn.Module):
+    def __init__(self, ng : torch.nn.Parameter, nf : torch.nn.Parameter, nw : torch.nn.Parameter, margin : torch.nn.Parameter, model_lambda : torch.nn.Parameter, r : float):
+        """_summary_
+
+        Args:
+            ng (torch.nn.Parameter): number of genuine signatures inside the mini-batch
+            nf (torch.nn.Parameter): number of forgeries signatures inside the mini-batch
+            nw (torch.nn.Parameter): number of mini-batches (writers) inside a batch
+            margin (torch.nn.Parameter): triplet loss margin
+            model_lambda (torch.nn.Parameter): control intraclass variation
+
+        """
+        super(Window_Loss, self).__init__()
+        # Hyperparameters
+        self.ng = ng
+        self.nf = nf
+        self.nw = nw
+        self.model_lambda = model_lambda
+        self.margin = margin
+        self.r
+        self.sdtw = soft_dtw.SoftDTW(True, gamma=5, normalize=False, bandwidth=0.1)
+
+    def forward(self, data, lens):
+        """ Loss de um batch
+
+        Args:
+            data (torch tensor): Batch composto por mini self.nw mini-batches. Cada mini-batch é composto por 1+ng+nf (padrão=16) assinaturas e se refere a um único usuário e é (no padrão) disposto da seguinte forma:
+            [0]: âncora; [1:ng+1]: amostras positivas; [ng+1:ng+1+nf//2]: falsificações profissionais; [ng+1+nf//2:]: aleatórias 
+            lens (torch tensor (int)): tamanho de cada assinatura no batch
+
+        Returns:
+            torch.tensor (float): valor da loss
+        """
+
+        step = (self.ng + self.nf + 1)
+        total_loss = 0
+        dists_gs = torch.zeros(self.ng*self.nw, dtype=data.dtype, device=data.device)
+        dists_ns = torch.zeros(self.nf*self.nw, dtype=data.dtype, device=data.device)
+
+        for i in range(0, self.nw):
+            anchor    = data[i * step]
+            positives = data[i * step + 1 : i * step + 1 + self.ng] 
+            negatives = data[i * step + 1 + self.ng : i * step + 1 + self.ng + self.nf]
+
+            len_a = lens[i * step]
+            len_p = lens[i * step + 1 : i * step + 1 + self.ng] 
+            len_n = lens[i * step + 1 + self.ng : i * step + 1 + self.ng + self.nf]
+
+            dist_g = torch.zeros((len(positives)), dtype=data.dtype, device=data.device)
+            dist_n = torch.zeros((len(negatives)), dtype=data.dtype, device=data.device)
+
+            '''Average_Pooling_2,4,6'''
+            for j in range(len(positives)): 
+                dist_g[j] = self.sdtw(anchor[None, :int(len_a)], positives[j:j+1, :int(len_p[j])])[0] / (len_a + len_p[j])
+                dists_gs[i*self.ng + j] = dist_g[j]
+
+            for j in range(len(negatives)):
+                dist_n[j] = self.sdtw(anchor[None, :int(len_a)], negatives[j:j+1, :int(len_n[j])])[0] / (len_a + len_n[j])
+                dists_ns[i*self.nf + j] = dist_n[j]
+    
+        smooth = 5
+
+        expg = torch.exp(smooth * dists_gs)
+        dp = torch.sum(dists_gs * expg)/torch.sum(expg)
+
+        expn = torch.exp(-smooth * dists_ns)
+        dn = torch.sum(dists_ns * expn)/torch.sum(expn)
+
+        window_loss = F.relu(dp - dn)
+
+        return window_loss
