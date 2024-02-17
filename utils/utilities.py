74520cb95211a08812bcd30ac58155363a04160a
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-02-17 18:25:43 -0300

    Add files via upload

--- utils/utilities.py
+++ utils/utilities.py
@@ -1,7 +1,7 @@
     
 import numpy as np
 from numpy import typing as npt
-from Losses import triplet_loss, triplet_mmd, compact_triplet_mmd, clustering_triplet_mmd, contrastive_loss, triplet_loss_offset, compact_triplet_mmd_offset, clustering_triplet_loss, hard_triplet_loss_avg, hard_triplet_loss_max, triplet_distillation, tune_loss, compact_triplet_mmd2, window_loss
+from Losses import triplet_loss, triplet_mmd, compact_triplet_mmd, clustering_triplet_mmd, contrastive_loss, triplet_loss_offset, compact_triplet_mmd_offset, clustering_triplet_loss, hard_triplet_loss_avg, hard_triplet_loss_max, triplet_distillation, tune_loss, compact_triplet_mmd2, window_loss, window_loss2
 from typing import List, Tuple, Dict, Any
 import torch
 import json
@@ -37,7 +37,7 @@ def define_loss(loss_type : str, ng : int, nf : int, nw : int, margin : torch.nn
     if loss_type.lower() == "window_loss":
         return window_loss.Window_Loss(ng=ng, nf=nf, nw=nw, margin=margin, model_lambda=model_lambda, r=r)
     if loss_type.lower() == "window_loss2":
-        return window_loss.Window_Loss(ng=ng, nf=nf, nw=nw, margin=margin, model_lambda=model_lambda, p=p)
+        return window_loss2.Window_Loss2(ng=ng, nf=nf, nw=nw, margin=margin, model_lambda=model_lambda, r=r, p=p)
     raise NameError("Loss function not found")
 
 def traceback(acc_cost_matrix : npt.DTypeLike):
