5e0b90086f26b7f9233755867ff52a998e19132c
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-02-27 16:17:21 -0300

    Add files via upload

--- run0.sh
+++ run0.sh
@@ -1,35 +1,11 @@
-result="CTL_S07_016_"
-for i in {001..035}
+result="CTL_S15"
+for i in {001..010}
 do
-    PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.9 -b 2.7 -p 0.57 -r 0.608 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -t=$result$i -pf=CTL_S06_016_ -wpn=CTL_S06
+	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -pf=$result -wpn=$result -t=$result$i -nout=32 -nh=96
 done
 
-# Gera o sumario dos treinamentos
-# python GerarMetricas/sumarizar.py ../"$result" Comp_DeepSignDB_skilled_stylus_4vs1
-# # Gera os boxplots
-# python GerarMetricas/boxplot.py ../"$result"/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv ../"$result"
-# # Concatena os resultados a fim de criar o csv para o blox plot em mesma escala
-# python GerarMetricas/concat_files.py "Todos/Resumo Todos.csv" ../"$result"/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv
-# # Gera o boxplot em mesma escala
-# python GerarMetricas/box2.py "Todos/Resumo Todos.csv" Todos
-
-
-
-# # Gera o sumario dos treinamentos
-# python3 GerarMetricas/sumarizar.py ../CTL_S06_016_105 Comp_DeepSignDB_skilled_stylus_4vs1
-# # Gera os boxplots
-# python GerarMetricas/boxplot.py ../CTL_S06_016_105/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv ../CTL_S06_016_105
-# # Concatena os resultados a fim de criar o csv para o blox plot em mesma escala
-# python GerarMetricas/concat_files.py "Todos/Resumo Todos.csv" ../CTL_S06_016_105/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv
-# # Gera o boxplot em mesma escala
-# python GerarMetricas/box105.py "Todos/Resumo Todos.csv" Todos
-
-
-# # Gera o sumario dos treinamentos
-# python3 GerarMetricas/sumarizar.py ../CTMMD_OFFSET_ Comp_DeepSignDB_skilled_stylus_4vs1
-# # Gera os boxplots
-# python3 GerarMetricas/boxplot.py ../CTMMD_OFFSET_/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv ../CTMMD_OFFSET_
-# # Concatena os resultados a fim de criar o csv para o blox plot em mesma escala
-# python3 GerarMetricas/concat_files.py "Todos/Resumo Todos.csv" ../CTMMD_OFFSET_/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv
-# # Gera o boxplot em mesma escala
-# python3 GerarMetricas/box2.py "Todos/Resumo Todos.csv" Todos
+result="CTL_S16"
+for i in {001..010}
+do
+	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -pf=$result -wpn=$result -t=$result$i -nout=64 -nh=96
+done
--- run1.sh
+++ run1.sh
@@ -1,14 +1,11 @@
-result="CTL_S07_016_"
-for i in {036..070}
+result="CTL_S17"
+for i in {001..010}
 do
-    PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.9 -b 2.7 -p 0.57 -r 0.608 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -t=$result$i -pf=CTL_S06_016_ -wpn=CTL_S06
+	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -pf=$result -wpn=$result -t=$result$i -nout=32 -nh=80
 done
 
-# Gera o sumario dos treinamentos
-python GerarMetricas/sumarizar.py ../"$result" Comp_DeepSignDB_skilled_stylus_4vs1
-# Gera os boxplots
-python GerarMetricas/boxplot.py ../"$result"/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv ../"$result"
-# Concatena os resultados a fim de criar o csv para o blox plot em mesma escala
-python GerarMetricas/concat_files.py "Todos/Resumo Todos.csv" ../"$result"/resumo_Comp_DeepSignDB_skilled_stylus_4vs1.csv
-# Gera o boxplot em mesma escala
-python GerarMetricas/box2.py "Todos/Resumo Todos.csv" Todos
+result="CTL_S18"
+for i in {001..010}
+do
+	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -pf=$result -wpn=$result -t=$result$i -nout=40 -nh=80
+done
