3eec663f01b1dae8f65d93c6a230db472a1409d0
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-03-09 09:14:53 -0300

    Add files via upload

--- run1.sh
+++ run1.sh
@@ -1,11 +1,5 @@
-result="CTL_S17"
-for i in {001..010}
+for i in {001..020}
 do
-	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -pf=$result -wpn=$result -t=$result$i -nout=32 -nh=80
-done
-
-result="CTL_S18"
-for i in {001..010}
-do
-	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -pf=$result -wpn=$result -t=$result$i -nout=40 -nh=80
+	result="ROT_X1_"
+	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -pf=$result -wpn=$result -t=$result$i
 done
--- run2.sh
+++ run2.sh
@@ -1,11 +1,5 @@
-for i in {001..010}
+for i in {001..020}
 do
-	result="ROT_S1_"
+	result="ROT_X1_"
 	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -pf=$result -wpn=$result -t=$result$i
-	
-	result="ROT_S2_"
-	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=triplet_mmd -a 1 -p 0.9 -r 0.1 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=32 -nw=2 -seed=333 -wdb -pf=$result -wpn=$result -t=$result$i
-
-	result="ROT_S3_"
-	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=triplet_loss -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -pf=$result -wpn=$result -t=$result$i
 done
