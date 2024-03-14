74ef286cd71567daa931872f2000d63eabaabde7
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-03-14 16:32:14 -0300

    Add files via upload

--- run1.sh
+++ run1.sh
@@ -1,5 +1,6 @@
+aw="_AW"
 for i in {001..020}
 do
-	result="ROT_X1_"
-	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -pf=$result -wpn=$result -t=$result$i
+	result="ROT_X2_"
+	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -aw -wdb -ep=25 -seed=333 -pf=$result -t=$result$i -wpn=$result$aw -es=3
 done
