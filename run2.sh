8e8078dc82252a95123d3d64e712f2c65d1c4b79
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-03-19 15:53:54 -0300

    Add files via upload

--- run2.sh
+++ run2.sh
@@ -1,5 +1,5 @@
 for i in {001..010}
 do
-	result="SLC_S1_"
-	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd -nsl=5 -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -pf=$result -wpn=$result -t=$result$i
+	result="RND_S1_"
+	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd_random -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -wdb -pf=$result -wpn=$result -t=$result$i
 done
