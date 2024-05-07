0804fee312b358be44aaf7637d9c240b7423735d
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-05-06 22:37:30 -0300

    Add files via upload

--- run0.sh
+++ run0.sh
@@ -1,5 +1,4 @@
-for i in {001..020}
-do
-	result="SCR_S1_"
-	PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=0 python run_ds_pipeline.py -lt=compact_triplet_mmd -a 0.96 -b 1.8 -p 0.809 -r 0.297 -lr 0.01 -ep=25 -stop=26 -es=3 -bs=64 -seed=333 -t=$result$i -pf=$result -wpn=SCR_S1_ -wdb
-done
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32 CUDA_VISIBLE_DEVICES=1 python run_ds_pipeline.py -sig="../Data/DeepSignDB/Evaluation/stylus" --extract_features=Evaluation -seed=333 -t="/Resultados/ROT_X2_/ROT_X2_005/" -pf=".." -w="epoch23.pt"
+            
+python Investigation/dba.py
+python Investigation/dba_distances.py
