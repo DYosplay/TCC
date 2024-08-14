a54e4c0dda8f8c6100a8ef37695f75bca9bc8d10
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-08-13 21:25:15 -0300

    Add files via upload

--- DataLoader/batches_gen.py
+++ DataLoader/batches_gen.py
@@ -3,13 +3,19 @@ import numpy as np
 import os
 import DataLoader.loader as loader
 from tqdm import tqdm
+from utils.pre_alignment import align
+from typing import Dict, Any
+import re
 
 def get_files(dataset_folder : str = "../Data/DeepSignDB/Development/stylus"):
     files = os.listdir(dataset_folder)
     users = {}
     for file in files:
         tokens = file.split('_')
-        key = tokens[0] + tokens[1]
+        if 'syn' in file:
+            key = tokens[0] + "syn" + tokens[1]
+        else:
+            key = tokens[0] + tokens[1]
         if key in users:
             users[key].append(dataset_folder + os.sep + file)
         else:
@@ -17,28 +23,38 @@ def get_files(dataset_folder : str = "../Data/DeepSignDB/Development/stylus"):
 
     return users
 
-def files2array(batch, scenario : str, z : bool, developtment : bool):
+def files2array(batch, hyperparameters : Dict[str,Any], z : bool, development : bool):
     data = []; lens = []
-    # scenario = None
-    # if developtment:
-    #     batch = batch[1:]
 
     for file in batch:
-        file = file.replace('\\', os.sep)
-        file = file.replace('/', os.sep)
-        if developtment == False and "Evaluation" in file: file = ".." + os.sep + "Data" + os.sep + "DeepSignDB" + os.sep + file.strip()
-        elif developtment == False: file = ".." + os.sep + "Data" + os.sep + "DeepSignDB" + os.sep + "Evaluation" + os.sep + scenario + os.sep + file.strip()
-        
-        # Se quiser testar usando o conjunto de treino
-        # if developtment == True: file = "Data" + os.sep + "DeepSignDB" + os.sep + "Development" + os.sep + "stylus" + os.sep + file
-
-        scenario = "stylus" if "stylus" in file else "finger"
-        
-        feat = loader.get_features(file, scenario=scenario, z=z, development=developtment)
+        feat = None
+        if '_syn_' in file:
+            with open(file,'r') as fr:
+                lines = fr.readlines()
+                lines = lines[1:]
+                feat = np.genfromtxt(lines, delimiter=',').T
+        else:
+            file = file.replace('\\', os.sep)
+            file = file.replace('/', os.sep)
+            file = file.strip()
+            
+            if hyperparameters['signature_path'] is not None:
+                file = os.path.join(hyperparameters['signature_path'], file)
+                development = 'Development' in file
+            else:
+                subset_folder = 'Development' if development else "Evaluation"
+                file_path = os.path.join(hyperparameters['dataset_folder'], subset_folder, hyperparameters['dataset_scenario'])
+                if file_path not in file: file = os.path.join(file_path, file)
+            
+            feat = loader.get_features(file, hyperparameters=hyperparameters, z=z, development=development)
         data.append(feat)
         lens.append(len(feat[0]))
 
     max_size = max(lens)
+
+    if hyperparameters['pre_alignment']:
+        data, lens = align(data,hyperparameters=hyperparameters)
+        max_size = max(lens)
 
     generated_batch = []
     for i in range(0, len(data)):
@@ -92,7 +108,7 @@ def get_batch_from_transfer_domain_epoch(epoch, batch_size : int):
     return data, lens, epoch
 
 """DeepSign"""
-def get_batch_from_epoch(epoch, batch_size : int, z : bool, scenario : str):
+def get_batch_from_epoch(epoch, batch_size : int, z : bool, hyperparameters : Dict[str,Any]):
     assert batch_size % 16 == 0
     step = batch_size // 16
 
@@ -100,7 +116,7 @@ def get_batch_from_epoch(epoch, batch_size : int, z : bool, scenario : str):
     for i in range(0, step):
         batch += epoch.pop()
 
-    data, lens = files2array(batch, scenario=scenario, z=z, developtment=True)
+    data, lens = files2array(batch, hyperparameters=hyperparameters, z=z, development=True)
 
     return data, lens, epoch
 
@@ -129,7 +145,7 @@ def generate_mixed_epoch(train_offset = [(1, 498), (1009, 1084)]):
     random.shuffle(epoch)
     return epoch
 
-def generate_epoch(dataset_folder : str = "../Data/DeepSignDB/Development/stylus", train_offset = [(1, 498), (1009, 1084)], users=None, development = True, scenario : str = 'stylus'):
+def generate_epoch(dataset_folder : str, hyperparameters : Dict[str, Any], train_offset = [(1, 498), (1009, 1084)], users=None, development = True):
     files = get_files(dataset_folder=dataset_folder)
     files_backup = files.copy()
 
@@ -143,34 +159,59 @@ def generate_epoch(dataset_folder : str = "../Data/DeepSignDB/Development/stylus
     epoch = []
     number_of_mini_baches = 0
 
+    multiplier = 1
+    if hyperparameters['rotation']:
+        multiplier = 2
+
     database = None
     print("Gererating new epoch")
 
     for user_id in tqdm(train_users):
         
-        database = loader.get_database(user_id=user_id, scenario=scenario, development=development)
+        database = loader.get_database(user_id=user_id, development=development, hyperparameters=hyperparameters)
 
         if database == loader.EBIOSIGN1_DS1 or database == loader.EBIOSIGN1_DS2:
-            number_of_mini_baches = 1
+            number_of_mini_baches = 1 * multiplier
         elif database == loader.MCYT or database == loader.BIOSECURE_DS2:
-            number_of_mini_baches = 4
+            number_of_mini_baches = 4 * multiplier
             # continue
         elif database == loader.BIOSECUR_ID:
-            number_of_mini_baches = 2
+            number_of_mini_baches = 2 * multiplier
             # continue
         else:
             raise ValueError("Dataset desconhecido!")
 
         for i in range(0, number_of_mini_baches):
+            genuines = []
+            syn_genuines = []
+            s_forgeries = []
+            syn_s_forgeries = []
+            n_random = 5
+            if hyperparameters['synthetic']:
+                genuines = random.sample(files['u' + f"{user_id:04}" + 'g'], 5)
+                files['u' + f"{user_id:04}" + 'g'] = list(set(files['u' + f"{user_id:04}" + 'g']) - set(genuines))
 
-            genuines = random.sample(files['u' + f"{user_id:04}" + 'g'], 6)
-            files['u' + f"{user_id:04}" + 'g'] = list(set(files['u' + f"{user_id:04}" + 'g']) - set(genuines))
+                syn_genuines = random.sample(files['u' + f"{user_id:04}" + 'syng'], 2)
+                files['u' + f"{user_id:04}" + 'g'] = list(set(files['u' + f"{user_id:04}" + 'syng']) - set(syn_genuines))
 
-            s_forgeries = random.sample(files['u' + f"{user_id:04}" + 's'], 5)
-            files['u' + f"{user_id:04}" + 's'] = list(set(files['u' + f"{user_id:04}" + 's']) - set(s_forgeries))
+                s_forgeries = random.sample(files['u' + f"{user_id:04}" + 's'], 4)
+                files['u' + f"{user_id:04}" + 's'] = list(set(files['u' + f"{user_id:04}" + 's']) - set(s_forgeries))
+
+                syn_s_forgeries = random.sample(files['u' + f"{user_id:04}" + 'syns'],2)
+                files['u' + f"{user_id:04}" + 's'] = list(set(files['u' + f"{user_id:04}" + 'syng']) - set(syn_s_forgeries))
+
+                n_random = 3
+            else:
+                genuines = random.sample(files['u' + f"{user_id:04}" + 'g'], hyperparameters['ng'])
+                files['u' + f"{user_id:04}" + 'g'] = list(set(files['u' + f"{user_id:04}" + 'g']) - set(genuines))
+
+                s_forgeries = random.sample(files['u' + f"{user_id:04}" + 's'], hyperparameters['nf']//2)
+                files['u' + f"{user_id:04}" + 's'] = list(set(files['u' + f"{user_id:04}" + 's']) - set(s_forgeries))
+
+            
 
             # ids aleatórios podem ser de qualquer mini dataset
-            random_forgeries_ids = list(set(random.sample(train_users, 6)) - set([user_id]))[:5]
+            random_forgeries_ids = list(set(random.sample(train_users, 6)) - set([user_id]))[:n_random]
             # ids aleatórios apenas do mesmo dataset
             # random_forgeries_ids = get_random_ids(user_id=user_id, database=database, samples=5)
 
@@ -179,8 +220,8 @@ def generate_epoch(dataset_folder : str = "../Data/DeepSignDB/Development/stylus
                 random_forgeries.append(random.sample(files_backup['u' + f"{id:04}" + 'g'], 1)[0])
 
             a = [genuines[0]]
-            p = genuines[1:6]
-            n = s_forgeries + random_forgeries
+            p = genuines[1:] + syn_genuines
+            n = s_forgeries + syn_s_forgeries + random_forgeries
 
             mini_batch = a + p + n
 
