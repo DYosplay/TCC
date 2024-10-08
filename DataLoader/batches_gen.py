d7263b11987b87ff0a875b915b330ffa9e71a4a1
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-10-08 19:48:10 -0300

    Add files via upload

--- DataLoader/batches_gen.py
+++ DataLoader/batches_gen.py
@@ -122,7 +122,7 @@ def get_batch_from_epoch(epoch, batch_size : int, z : bool, hyperparameters : Di
         batch += b
         for j in range(0, len(b)):
             id = int(b[j].split(os.sep)[-1].split('_')[0].split('u')[1]) - 1
-            if id >= 1009: id -= 511
+            if id >= 1008: id -= 511
             if '_s_' in b[j]: id += 574 # falsificacao profissional
             users.append(id)
 
