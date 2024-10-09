01159d366e8d0c85c519ecc3c2583f99b8d0b2df
João Pedro Felix <67055531+DYosplay@users.noreply.github.com>
2024-10-09 16:52:03 -0300

    versao sem chamar a triplet mmd

--- ds_pipeline.py
+++ ds_pipeline.py
@@ -729,15 +729,22 @@ class DsPipeline(nn.Module):
                 
                 # prob = self.soft_min(predict)
                 loss2 = self.cross_entropy_loss(predict, targets)
+                # loss2.backward(retain_graph=True)
+                loss2.backward()
+                
+                loss = loss2         # apenas uma gambiarra
                 # loss2 = torch.nn.functional.cross_entropy(prob, targets)
                 # targets = targets.unsqueeze(1).unsqueeze(2).expand(output2.shape).float()
-                loss, nonzero = self.loss_function(outputs, length)
+                
                 
                 # loss, nonzero = self.loss_function(outputs, length, targets, self.n_classes)
-                self.non_zero_random += nonzero
                 # loss = self.loss_function(outputs, length, w1, w2)
+
+                """Essa eh a versao que eu uso junto com lce e que funciona sozinha"""
+                # loss, nonzero = self.loss_function(outputs, length)
+                # self.non_zero_random += nonzero
                 # loss.backward()
-                (loss + loss2).backward()
+                
 
                 optimizer.step()
                 if self.hyperparameters['ga']: self.p.data.clamp_(0.0,1.0)
@@ -745,6 +752,9 @@ class DsPipeline(nn.Module):
                 lce_acc += loss2.item()
                 tm_acc += loss.item()
                 running_loss += loss.item()
+
+                del loss
+                del loss2
             
                 pbar.update(1)
 
